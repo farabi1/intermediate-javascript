2bd3a2bbfec1bc8d787428deec85961b5f10b8f7
farabi1 <rashidfarabi@gmail.com>
2022-03-02 21:07:17 +0600

    Bonus Part

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const allPhone = () => {
     document.getElementById('phone-container').innerHTML = "";
+    document.getElementById('details-container').innerHTML = "";
 
     const searchText = document.getElementById("search-box").value;
 
@@ -67,14 +68,15 @@ const detailsData = (info) => {
     document.getElementById("details-container").innerHTML = `
         <div>
             <img src="${info.image}" alt="">
-        <h4>Storage:${info.mainFeatures.storage}</h4>
-        <h5>Display:${info.mainFeatures.displaySize}</h5>
-        <h6>Chipset:${info.mainFeatures.chipset}</h6>
-        <h6>Memory:${info.memory}</h6>
-        <h6>Sensor:${info.mainFeatures.sensors.join()}</h6>
-        <h6>Others:Bluetooth-${info.others.Bluetooth},GPS-${info.others.GPS},NFC-${info.others.NFC},Radio-${info.others.Radio},USB-${info.others.USB},WLAN-${info.others.WLAN}</h6>
+        <h3>Name: ${info.name}</h3>    
+        <h4>Brand: ${info.brand}</h4>    
+        <h4>Storage: ${info.mainFeatures.storage}</h4>
+        <h5>Display: ${info.mainFeatures.displaySize}</h5>
+        <h6>Chipset: ${info.mainFeatures.chipSet}</h6>
+        <h6>Memory: ${info.mainFeatures.memory}</h6>
+        <h6>Sensor: ${info.mainFeatures.sensors.join()}</h6>
+        <h6>Others: Bluetooth-${info.others.Bluetooth},GPS-${info.others.GPS},NFC-${info.others.NFC},Radio-${info.others.Radio},USB-${info.others.USB},WLAN-${info.others.WLAN}</h6>
         <h6>Release Date:${info.releaseDate}</h6>
-        <h6>Others:${info.something ? info.something.others : "no result found"}</h6>
     </div>
    `
     console.log(info)
